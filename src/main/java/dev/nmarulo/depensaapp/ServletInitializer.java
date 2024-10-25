ebb76cbe89a8be5960db2b905fbc95358842ab26
Anto Christopher <93177734+anto-deepsource@users.noreply.github.com>
2024-10-25 09:54:07 +0530

    Update ServletInitializer.java

--- src/main/java/dev/nmarulo/depensaapp/ServletInitializer.java
+++ src/main/java/dev/nmarulo/depensaapp/ServletInitializer.java
@@ -7,6 +7,11 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        // This expression can be simplified to directly compare the primitive values instead.
+        Integer.valueOf(3).compareTo(2)
+        
+        // or
+        new Integer(3).compareTo(2)
         return application.sources(DespensaRestApiApplication.class);
     }
 
