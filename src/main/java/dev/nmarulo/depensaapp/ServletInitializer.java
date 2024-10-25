311470edc4b011dfb90ac1d529493bb3be8a5d74
Anto Christopher <93177734+anto-deepsource@users.noreply.github.com>
2024-10-25 09:59:40 +0530

    Update ServletInitializer.java

--- src/main/java/dev/nmarulo/depensaapp/ServletInitializer.java
+++ src/main/java/dev/nmarulo/depensaapp/ServletInitializer.java
@@ -3,6 +3,8 @@ package dev.nmarulo.depensaapp;
 import java.util.HashMap;
 import java.util.Map;
 import java.net.URL;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.ConcurrentSkipListMap;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
@@ -16,6 +18,10 @@ public class ServletInitializer extends SpringBootServletInitializer {
         for (HashMap.Entry<URL, Integer> e : hits) {
             // ... This can become very slow for larger hashmaps of URLS.
         }
+        final ConcurrentMap<Integer, Integer> m = new ConcurrentSkipListMap<>();
+        m.put(3 ,4);
+        m.put(4, 2);
+        m.get(4);
         return application.sources(DespensaRestApiApplication.class);
     }
 
